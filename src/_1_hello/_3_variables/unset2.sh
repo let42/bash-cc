1ffdfdfe56f42deaed263672cc0ed2906783c15b
let42 <alberto@gucciardi.xyz>
2017-11-19 00:31:54 +0100

    Created two simple files sh that teach you how to initialize vars in shell scripting

---
+++ src/_1_hello/_3_variables/unset2.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# this script shows how a variable could be used and called
+
+# remind that echo is the output on terminal
+
+#echo "Your name is $YOUR_NAME" # dollar sign is used to call a variable
+YOUR_NAME=Alberto
+echo "Your name is $YOUR_NAME"
+read YOUR_NAME
+echo "How could be?"
+echo $YOUR_NAME$
+
