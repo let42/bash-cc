3199a425383547637807f1e345ddef7d65fc9408
let42 <alberto@gucciardi.xyz>
2017-11-19 00:54:13 +0100

    requesting from shell first name and lastname of the user

---
+++ src/_3_variables/_first_and_last_name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#type your firstname and last name from prompt
+
+echo -n "Insert your first and last name ~> "
+read _first_name _last_name
+echo "Hi $_first_name. How is $_last_name's family?"
